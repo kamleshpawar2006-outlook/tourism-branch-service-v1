2f5dd4fee44c42bb93a30b6e3656e62c0fb51188
kamleshpawar2006 <kamleshpawar2006@gmail.com>
2024-09-21 12:29:35 +0100

    swagger implementation in inner API

--- src/main/java/com/tm/controller/TouristCompanyController.java
+++ src/main/java/com/tm/controller/TouristCompanyController.java
@@ -3,6 +3,8 @@ package com.tm.controller;
 import com.tm.dto.TariffDto;
 import com.tm.dto.TouristCompanyDto;
 import com.tm.service.TouristCompanyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -11,20 +13,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("/tourism/branch")
+@RequestMapping("/tourism/api/v1/branch")
 @Validated
 public class TouristCompanyController {
     @Autowired
     private TouristCompanyService touristCompanyService;
 
+    @Operation(summary = "Adds a tourist company with tariff details",
+            description = "Add a new tourist company along with the places it operates in.")
     @PostMapping("/add-places")
-    public Map<String, Long> addTouristCompany(@Valid @RequestBody TouristCompanyDto company) {
+    public Map<String, Long> addTouristCompany(@Parameter(description = "Tourist Company details", required = true) @Valid @RequestBody TouristCompanyDto company) {
         return touristCompanyService.addTouristCompany(company);
     }
 
+    @Operation(summary = "Updates the tariff details of a tourist company",
+            description = "Updates the tariff details of a tourist company")
     @PutMapping("/update-tariff/{branchId}")
-    public TouristCompanyDto updateTariff(@PathVariable Long branchId,@Valid @RequestBody List<TariffDto> tariffs) {
+    public TouristCompanyDto updateTariff(@Parameter(description = "List of new tariffs", required = true) @PathVariable Long branchId,@Valid @RequestBody List<TariffDto> tariffs) {
         return touristCompanyService.updateTariff(branchId, tariffs);
     }
 }
