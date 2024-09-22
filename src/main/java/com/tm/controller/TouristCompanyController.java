375df134131303acbfa2f80e38ac81d5fc2f4320
kamleshpawar2006 <kamleshpawar2006@gmail.com>
2024-09-22 19:32:02 +0100

    eureka,individual swagger, CQRS, combined swagger with error

--- src/main/java/com/tm/controller/TouristCompanyController.java
+++ src/main/java/com/tm/controller/TouristCompanyController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/tourism/api/v1/branch")
 @Validated
